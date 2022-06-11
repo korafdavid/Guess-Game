e880e9528720a2979aacba92d459ced9ff8820c5
David okoroafor <>
2022-06-11 14:27:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Welcome to Guess Game. press q to quit");
+startGame(2)
+
+
+
+function startGame( range) {
+const username = prompt('Please type your username');
+var point = 0;
+while(true) {
+  var guessInput = prompt(`guess a number between 1 and ${range}`);
+ var random = Math.round(Math.random() * (range - 1) + 1);
+  console.log(random)
+  if(random == guessInput){
+    point++;
+    range++
+  } 
+  if(guessInput == "q"){
+    console.log(`${username} your total point is ${point}`)
+    break;
+  }
+}
+
+}
